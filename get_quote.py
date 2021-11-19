ccc6005a91f3f5dc7891a45ac3695c483100800e
Erwin Kragt <ekragt@gmail.com>
2021-11-19 10:30:36 +0100

    Update get_quote.py

--- get_quote.py
+++ get_quote.py
@@ -9,9 +9,9 @@ def primary():
     "primary function"
     print("Keep it logically awesome.")
 
-    quote_file = open("quotes.txt", encoding="latin-1")
-    quotes = quote_file.readlines()
-    quote_file.close()
+    with open("quotes.txt", encoding="latin-1") as quote_file :
+        quotes = quote_file.readlines()
+        quote_file.close()
 
     last = 13
     rnd = random.randint(0, last)
